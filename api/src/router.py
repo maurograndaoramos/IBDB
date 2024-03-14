ed2cbec12275ca91f7885090b749b9d79f6bdd2f
ricardoAlmeidaEtic <ricardo_almeida@gmail.com>
2024-03-14 22:04:22 +0000

    adição da class user na api.

--- api/src/router.py
+++ api/src/router.py
@@ -1,17 +1,32 @@
 from fastapi import APIRouter, Request
 
-
 api_router = APIRouter(
     prefix="/api",
     tags=["api"],
 )
 
+class User(BaseModel):
+    user_id: int
+    username: str
+    email: str
+    isAdmin: int
+    hash: str
 
-@api_router.get("/")
+@api_router.get("/users")
 async def get_call(_: Request):
     return {"message": "You successfully GET called the API!"}
 
+@app.post("/create_user/")
+async def create_user(user_data: UserCreate):
+    user_id = user_data.user_id
+    username = user_data.username
+    return {
+        "msg": "we got data succesfully",
+        "user_id": user_id,
+        "username": username,
+    }
 
-@api_router.post("/")
+
+@api_router.post("/company")
 async def post_call(_: Request):
     return {"message": "You successfully POST called the API!"}
